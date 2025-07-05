a2141db9a1852ea19ebc1f1f3846349e47d19aa7
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2025-07-05 00:58:23 -0400

    directory setup

---
+++ Backend/FindConcertPal/src/main/java/com/smaran/findconcertpal/FindConcertPalApplication.java
@@ -0,0 +1,13 @@
+package com.smaran.findconcertpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindConcertPalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindConcertPalApplication.class, args);
+    }
+
+}
---
+++ Backend/FindConcertPal/src/test/java/com/smaran/findconcertpal/FindConcertPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smaran.findconcertpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindConcertPalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
