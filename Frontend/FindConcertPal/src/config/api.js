1bff36d3f16361fef5e38d839da09c9ce75ab158
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2025-07-17 05:36:19 -0400

    Axios intance setup

---
+++ Frontend/FindConcertPal/src/config/api.js
@@ -0,0 +1,22 @@
+export const API_BASE_URL= "http://localhost:8080"
+
+const api = axios.create({
+  baseURL: API_BASE_URL,})
+
+api.interceptors.request.use(
+  (config) => {
+    const jwt = localStorage.getItem("jwt");
+    if (jwt) {
+      config.headers.Authorization = `Bearer ${jwt}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);  
+
+
+api.defaults.headers.post['Content-Type'] = 'application/json';
+
+export default api;
