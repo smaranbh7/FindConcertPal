74d3a56d59f2b47983e8eef41b9830a984ba1740
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2025-07-19 01:35:56 -0400

    Internal error fix

--- Frontend/FindConcertPal/src/main.jsx
+++ Frontend/FindConcertPal/src/main.jsx
@@ -2,13 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { store } from './store/Store'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-    
+    <App />
   </StrictMode>,
 )
