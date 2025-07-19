a31656fa4ad71da84b5d996f527338ab7065384d
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2025-07-19 01:53:37 -0400

    Fixed redux bugs

---
+++ Frontend/FindConcertPal/src/redux/Store.js
@@ -0,0 +1,11 @@
+import { authReducer } from "./auth/Reducer"
+
+import { applyMiddleware, combineReducers, legacy_createStore } from "redux"
+import { thunk  } from "redux-thunk"
+
+const rootReducer= combineReducers({
+    auth: authReducer
+
+});
+
+export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
