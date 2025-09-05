6560ce9518f2c782800d29c32480f33e4e7c4e01
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2025-09-04 22:18:17 -0400

    Fav genres displaying

--- Backend/FindConcertPal/src/main/java/com/smaran/findconcertpal/controller/UserController.java
+++ Backend/FindConcertPal/src/main/java/com/smaran/findconcertpal/controller/UserController.java
@@ -6,7 +6,6 @@ import com.smaran.findconcertpal.model.User;
 import com.smaran.findconcertpal.repo.UserRepo;
 import com.smaran.findconcertpal.response.ServerResponse;
 import com.smaran.findconcertpal.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
